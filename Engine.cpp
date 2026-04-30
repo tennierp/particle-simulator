f418a57633d1ba965ca68f0b249d6a9b8ce81fe3
Parker Tennier <227867231+tennierp@users.noreply.github.com>
2026-04-30 16:07:25 -0700

    Update Engine class, broke up the run function to multiple functions

--- Engine.cpp
+++ Engine.cpp
@@ -1,37 +1,63 @@
 #include "Engine.h"
 
-int WINDOW_WIDTH = 640;
-int WINDOW_HEIGHT = 480;
-
 void Engine::run() {
+    initWindow();
+    loop();
+    cleanup();
+}
+
+void Engine::initWindow() {
     SDL_Init(SDL_INIT_VIDEO);
     window = SDL_CreateWindow("Particles of Life", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
-    renderer = SDL_CreateRenderer(window, nullptr);
+    renderer = SDL_CreateRenderer(window, nullptr); // renderer will be moved into its own class but for now it lives here
 
     if (window == nullptr) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not open window: %s\n", SDL_GetError());
     }
+}
 
+void Engine::loop() {
+    // handle events → update simulation → render
     while (running) {
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-        }
-
-        // Clear screen
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        // Engine logic here
-
-
-
-        SDL_RenderPresent(renderer);
+        handleEvents();
+        // update() the particles will go here
+        render();
     }
+}
 
+void Engine::render() {
+    // Clear screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Engine logic here
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    // Draw screen
+    SDL_RenderPresent(renderer);
+}
+
+void Engine::cleanup() {
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void Engine::handleEvents() {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_EVENT_QUIT:
+                running = false;
+                break;
+
+            case SDL_EVENT_KEY_DOWN:
+                switch (event.key.key) {
+                    case SDLK_ESCAPE:
+                        running = false;
+                        break;
+                }
+        }
+    }
 }
--- Engine.h
+++ Engine.h
@@ -4,12 +4,21 @@
 
 class Engine {
     public:
-        bool running = true;
+        const int WINDOW_WIDTH = 640;
+        const int WINDOW_HEIGHT = 480;
+
+        void initWindow();
         void run();
+        void loop();
+        void render();
+        void cleanup();
+        void handleEvents();
 
     private:
         SDL_Window *window{};
         SDL_Renderer *renderer{};
+
+        bool running = true;
 };
 
 #endif //PARTICLE_SIMULATOR_ENGINE_H
