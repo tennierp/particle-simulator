7865b69446616c61968cc1b50460fafd17316671
Parker Tennier <227867231+tennierp@users.noreply.github.com>
2026-05-01 21:59:50 -0700

    Add Engine and Render class deconstructors

--- Engine.h
+++ Engine.h
@@ -1,26 +1,26 @@
 #ifndef PARTICLE_SIMULATOR_ENGINE_H
 #define PARTICLE_SIMULATOR_ENGINE_H
 #include <SDL3/SDL.h>
+#include "Render.h"
 #include "World.h"
 
 class Engine {
     public:
+        SDL_Window *window{};
         const int window_width = 1024;
         const int window_height = 720;
 
         void init();
         void run();
         void loop();
-        void render();
-        void cleanup();
+        void cleanup() const;
         void handleEvents();
 
-    private:
-        SDL_Window *window{};
-        SDL_Renderer *renderer{};
-        std::vector<SDL_FRect> rects;
+        ~Engine();
 
+    private:
         World world{(float)window_width, (float)window_height, 100};
+        Render renderer;
         bool running = true;
 };
 
